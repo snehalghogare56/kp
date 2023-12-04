56f2a49d1212b2b6d7f6877a0827aa2003990f40
snehalghogare56 <118335168+snehalghogare56@users.noreply.github.com>
2023-12-04 22:36:09 +0530

    Create nought.py

---
+++ nought.py
@@ -0,0 +1,56 @@
+Automatic nought and crossess using random number
+import random
+
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 9)
+
+def check_winner(board, player):
+    for row in board:
+        if all(cell == player for cell in row):
+            return True
+
+    for col in range(3):
+        if all(board[row][col] == player for row in range(3)):
+            return True
+
+    if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
+        return True
+
+    return False
+
+def is_board_full(board):
+    return all(cell != ' ' for row in board for cell in row)
+
+def play_game():
+    board = [[' ' for _ in range(3)] for _ in range(3)]
+    current_player = 'X'
+
+    while True:
+        print_board(board)
+
+        if current_player == 'X':
+            print("Player X's turn:")
+            row, col = map(int, input("Enter row and column (e.g., 1 2): ").split())
+        else:
+            print("Player O's turn:")
+            row, col = random.randint(0, 2), random.randint(0, 2)
+
+        if 0 <= row < 3 and 0 <= col < 3 and board[row][col] == ' ':
+            board[row][col] = current_player
+            if check_winner(board, current_player):
+                print_board(board)
+                print(f"Player {current_player} wins!")
+                break
+            elif is_board_full(board):
+                print_board(board)
+                print("It's a tie!")
+                break
+            else:
+                current_player = 'O' if current_player == 'X' else 'X'
+        else:
+            print("Invalid move. Try again.")
+
+if __name__ == "__main__":
+    play_game()
